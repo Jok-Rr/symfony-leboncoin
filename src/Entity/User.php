f2f9c538291524ac6485c52c419638a28fb84a20
Tom Bost <tom.bost.pro@gmail.com>
2022-06-24 16:45:20 +0200

    Feat(System): Add User Controller & View

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -6,115 +6,177 @@ use App\Repository\UserRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
-class User
+#[UniqueEntity(fields: ['username'], message: 'There is already an account with this username')]
+class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
-  #[ORM\Id]
-  #[ORM\GeneratedValue]
-  #[ORM\Column(type: 'integer')]
-  private $id;
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
 
-  #[ORM\Column(type: 'string', length: 255)]
-  private $username;
+    #[ORM\Column(type: 'string', length: 180, unique: true)]
+    private $username;
 
-  #[ORM\Column(type: 'string', length: 255)]
-  private $password;
+    #[ORM\Column(type: 'json')]
+    private $roles = [];
 
-  #[ORM\Column(type: 'datetime_immutable')]
-  private $created_at;
+    #[ORM\Column(type: 'string')]
+    private $password;
 
-  #[ORM\Column(type: 'datetime_immutable')]
-  private $updated_at;
+    #[ORM\Column(type: 'datetime_immutable')]
+    private $created_at;
 
-  #[ORM\OneToMany(mappedBy: 'user', targetEntity: ad::class, orphanRemoval: true)]
-  private $ads;
+    #[ORM\Column(type: 'datetime_immutable')]
+    private $updated_at;
 
-  public function __construct()
-  {
-      $this->ads = new ArrayCollection();
-  }
+    #[ORM\OneToMany(mappedBy: 'user', targetEntity: Ad::class, orphanRemoval: true)]
+    private $ads;
 
-  public function getId(): ?int
-  {
-    return $this->id;
-  }
+    public function __construct()
+    {
+        $this->ads = new ArrayCollection();
+    }
 
-  public function getUsername(): ?string
-  {
-    return $this->username;
-  }
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
 
-  public function setUsername(string $username): self
-  {
-    $this->username = $username;
+    /**
+     * @deprecated since Symfony 5.3, use getUserIdentifier instead
+     */
+    public function getUsername(): string
+    {
+        return (string) $this->username;
+    }
 
-    return $this;
-  }
+    public function setUsername(string $username): self
+    {
+        $this->username = $username;
 
-  public function getPassword(): ?string
-  {
-    return $this->password;
-  }
+        return $this;
+    }
 
-  public function setPassword(string $password): self
-  {
-    $this->password = $password;
+    /**
+     * A visual identifier that represents this user.
+     *
+     * @see UserInterface
+     */
+    public function getUserIdentifier(): string
+    {
+        return (string) $this->username;
+    }
 
-    return $this;
-  }
+    /**
+     * @see UserInterface
+     */
+    public function getRoles(): array
+    {
+        $roles = $this->roles;
+        // guarantee every user at least has ROLE_USER
+        $roles[] = 'ROLE_USER';
 
-  public function getCreatedAt(): ?\DateTimeImmutable
-  {
-    return $this->created_at;
-  }
+        return array_unique($roles);
+    }
 
-  public function setCreatedAt(\DateTimeImmutable $created_at): self
-  {
-    $this->created_at = $created_at;
+    public function setRoles(array $roles): self
+    {
+        $this->roles = $roles;
 
-    return $this;
-  }
+        return $this;
+    }
 
-  public function getUpdatedAt(): ?\DateTimeImmutable
-  {
-    return $this->updated_at;
-  }
+    /**
+     * @see PasswordAuthenticatedUserInterface
+     */
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
 
-  public function setUpdatedAt(\DateTimeImmutable $updated_at): self
-  {
-    $this->updated_at = $updated_at;
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
 
-    return $this;
-  }
+        return $this;
+    }
 
-  /**
-   * @return Collection<int, ad>
-   */
-  public function getAds(): Collection
-  {
-      return $this->ads;
-  }
+    /**
+     * Returning a salt is only needed, if you are not using a modern
+     * hashing algorithm (e.g. bcrypt or sodium) in your security.yaml.
+     *
+     * @see UserInterface
+     */
+    public function getSalt(): ?string
+    {
+        return null;
+    }
 
-  public function addAd(ad $ad): self
-  {
-      if (!$this->ads->contains($ad)) {
-          $this->ads[] = $ad;
-          $ad->setUser($this);
-      }
+    /**
+     * @see UserInterface
+     */
+    public function eraseCredentials()
+    {
+        // If you store any temporary, sensitive data on the user, clear it here
+        // $this->plainPassword = null;
+    }
 
-      return $this;
-  }
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->created_at;
+    }
 
-  public function removeAd(ad $ad): self
-  {
-      if ($this->ads->removeElement($ad)) {
-          // set the owning side to null (unless already changed)
-          if ($ad->getUser() === $this) {
-              $ad->setUser(null);
-          }
-      }
+    public function setCreatedAt(\DateTimeImmutable $created_at): self
+    {
+        $this->created_at = $created_at;
 
-      return $this;
-  }
+        return $this;
+    }
+
+    public function getUpdatedAt(): ?\DateTimeImmutable
+    {
+        return $this->updated_at;
+    }
+
+    public function setUpdatedAt(\DateTimeImmutable $updated_at): self
+    {
+        $this->updated_at = $updated_at;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, ad>
+     */
+    public function getAds(): Collection
+    {
+        return $this->ads;
+    }
+
+    public function addAd(ad $ad): self
+    {
+        if (!$this->ads->contains($ad)) {
+            $this->ads[] = $ad;
+            $ad->setUser($this);
+        }
+
+        return $this;
+    }
+
+    public function removeAd(ad $ad): self
+    {
+        if ($this->ads->removeElement($ad)) {
+            // set the owning side to null (unless already changed)
+            if ($ad->getUser() === $this) {
+                $ad->setUser(null);
+            }
+        }
+
+        return $this;
+    }
 }
